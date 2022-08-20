e04efce6de60526ac9e390282842be0318ae4b73
Ved <vedpat3@gmail.com>
2022-08-20 19:34:49 +0530

    Fixed recent issue with array and container function docs and handling of applied library docs

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -7,6 +7,12 @@ cd "/home/$user/project" || exit
 
 # installing requirements
 pip3 install -r requirements.txt
+if [[ -f optional.txt ]]
+then
+    pip3 install torch==1.11.0
+    pip3 install torch-scatter==2.0.9
+    pip3 install -r optional.txt
+fi
 
 # moving to the docs folder
 cd "docs" || exit
