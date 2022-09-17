fab5d979adf558354b73c1782dd77dbdbe579e25
Ved Patwardhan <54766411+VedPatwardhan@users.noreply.github.com>
2022-09-17 11:53:20 +0530

    Updated entrypoint.sh to terminate pipeline on error installing requirements

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -6,19 +6,19 @@ user="${working_directory[2]}"
 cd "/home/$user/project" || exit
 
 # installing requirements
-pip3 install -r requirements/requirements.txt
+pip3 install -r requirements/requirements.txt || exit 1
 if [[ -f requirements/optional.txt ]]
 then
     pip3 install torch==1.11.0
     pip3 install torch-scatter==2.0.9
-    pip3 install -r requirements/optional.txt
+    pip3 install -r requirements/optional.txt || exit 1
 fi
 
 # moving to the docs folder
 cd "docs" || exit
 
 # syncing current folder with the container's result folder to get results in this folder
-rsync -rav /home/"$user"/global_docs/* .
+rsync -rav /home/"$user"/global_docs/* . || exit 1
 
 # generate the documentation
 ./_make_docs.sh "$@"
