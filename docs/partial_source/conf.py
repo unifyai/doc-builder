1d27c102cb9722fdef50cdd93d92b68be5efc919
vedpatwardhan <vedpat3@gmail.com>
2023-01-20 10:49:59 +0530

    Updated copyright in the footer

--- docs/partial_source/conf.py
+++ docs/partial_source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Ivy'
-copyright = '2020-2022, Ivy Team'
+copyright = '2020-2023, Ivy Team'
 author = 'Ivy Team'
 
 # The full version, including alpha/beta/rc tags
