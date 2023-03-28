08e4432748498b0dfb528d8c1cf8cf824c16b663
Kareem Morsy <kreem.morsy@hotmail.com>
2023-03-28 15:17:52 +0000

    Add functionality to skip members

--- docs/_ext/skippable_function.py
+++ docs/_ext/skippable_function.py
@@ -3,26 +3,29 @@ from docutils.statemachine import StringList
 from sphinx.application import Sphinx
 from sphinx.ext.autodoc import MethodDocumenter
 
+def _skip(obj, skippable_method_attributes):
+    return any(
+        [
+            all(
+                [
+                    getattr(obj, key, None) == value
+                    for key, value in match.items()
+                ]
+            )
+            for match in skippable_method_attributes
+        ]
+    )
+
+def skip_method(app: Sphinx, what, name, obj, skip, options):
+    return skip or _skip(obj, app.config.skippable_method_attributes)
+
 class SkippableMethodDocumenter(MethodDocumenter):
     objtype = 'skippablemethod'
     directivetype = MethodDocumenter.objtype
     priority = MethodDocumenter.priority - 10
 
-    def skip(self) -> bool:
-        return any(
-            [
-                all(
-                    [
-                        getattr(self.object, key, None) == value
-                        for key, value in match.items()
-                    ]
-                )
-                for match in self.env.config.skippable_method_attributes
-            ]
-        )
-
     def add_directive_header(self, sig: str) -> None:
-        if self.skip():
+        if _skip(self.object, self.env.config.skippable_method_attributes):
             return
 
         super().add_directive_header(sig)
@@ -30,7 +33,7 @@ class SkippableMethodDocumenter(MethodDocumenter):
     def add_content(self,
                     more_content: StringList | None
                     ) -> None:
-        if self.skip():
+        if _skip(self.object, self.env.config.skippable_method_attributes):
             return
 
         super().add_content(more_content)
@@ -39,6 +42,7 @@ class SkippableMethodDocumenter(MethodDocumenter):
 def setup(app: Sphinx):
     app.setup_extension("sphinx.ext.autodoc")
     app.add_autodocumenter(SkippableMethodDocumenter)
+    app.connect("autodoc-skip-member", skip_method)
     app.add_config_value("skippable_method_attributes", [], "env")
 
     return {
