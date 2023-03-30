a1e5baf7647f6ca88d55e7416208f0ea89c477f4
Kareem Morsy <kreem.morsy@hotmail.com>
2023-03-30 12:40:18 +0000

    Added sphinx.ext.githubpages

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.viewcode",
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon",
+    "sphinx.ext.githubpages",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
     "docs._ext.custom_autosummary",
@@ -60,8 +61,6 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-
-html_extra_path = ["_html_extra"]
 
 html_css_files = [
     "css/custom.css",
