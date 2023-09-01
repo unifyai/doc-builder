94b5beeed80bfaf189ac177ed215c66e94f43c45
Kareem Morsy <kreem.morsy@hotmail.com>
2023-09-01 11:29:44 +0300

    feat: Update kapa.ai widget script

--- docs/_static/js/kapa.ai.js
+++ docs/_static/js/kapa.ai.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var script = document.createElement("script");
-  script.src = "https://widget.kapa.ai/kapa-widget.bundle.js";
+  script.src = "https://widget.kapa.ai/kapa-widget-v1.1.0.bundle.js";
   script.setAttribute("data-website-id", "ae98eb61-26c6-4078-8726-ef79982fed49");
   script.setAttribute("data-project-name", "Ivy");
   script.setAttribute("data-project-color", "#3BA83B");
